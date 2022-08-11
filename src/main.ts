79d680cb8e07f253c47adf750bb59a40c61d242f
ysrckr <dyt.yasarcakir@gmail.com>
2022-08-11 22:39:16 +0300

    project completed

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const getNewAdvice = axios.create({
 const getNewAdviceButton: HTMLButtonElement = document.querySelector('.btn')!
 const adviceText: HTMLParagraphElement = document.querySelector('.quotes')!
 const adviceTitle: HTMLHeadingElement = document.querySelector('.title')!
+const divider: HTMLImageElement = document.querySelector('.divider')!
 
 const displayNewAdvice = async () => {
 	const res = await getNewAdvice.get('/advice')
@@ -17,6 +18,24 @@ const displayNewAdvice = async () => {
 	adviceTitle.innerText = 'ADVICE #' + (await res.data.slip.id)
 }
 
-displayNewAdvice()
+const checkScreenSize = () => {
+	if (window.innerWidth < 768) {
+		divider.src = '../src/images/pattern-divider-mobile.svg'
+	} else {
+		divider.src = '../src/images/pattern-divider-desktop.svg'
+        divider.style.width = '100%'
+	}
+}
 
 getNewAdviceButton.addEventListener('click', displayNewAdvice)
+
+addEventListener('resize', () => {
+	if (innerWidth < 576) {
+		return
+	}
+	divider.src = '../src/images/pattern-divider-desktop.svg'
+	divider.style.width = '100%'
+})
+
+displayNewAdvice()
+checkScreenSize()
